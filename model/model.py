1d4eb79f6853359f60bce2335c8f48aa5c58f8f1
LorenzoIannicelli <s341841@studenti.polito.it>
2026-01-18 14:44:37 +0100

    ES. 2: da correggere

--- model/model.py
+++ model/model.py
@@ -33,36 +33,39 @@ class Model:
     def get_percorso(self, threshold):
 
         for n in self._G.nodes():
-            self._ricorsione([n], [n], threshold)
+            self._ricorsione([n], [], threshold)
 
         return self._percorso, self._max_costo
 
-    def _ricorsione(self, parziale, visited, threshold):
-        peso = self._calcola_peso(parziale)
-        if peso > self._max_weight:
-            self._max_weight = peso
-            self._percorso = copy.deepcopy(parziale)
+    def _ricorsione(self, parziale, parziale_archi, threshold):
+        disponibili = self.get_disponibili(parziale[-1], parziale_archi, threshold)
+
+        if len(disponibili) == 0:
+            costo = self._calcola_peso(parziale_archi)
+            if costo > self._max_costo:
+                self._percorso = parziale_archi.copy()
+                self._max_costo = costo
             return
 
-        v = parziale[-1]
+        for n1 in disponibili:
+            parziale.append(n1)
+            parziale_archi.append((parziale[-1], n1, self._G.get_edge_data(parziale[-1], n1)))
+            self._ricorsione(parziale, parziale_archi, threshold)
+            parziale.pop()
+            parziale_archi.pop()
 
-        for u in self._G.neighbors(v):
-            if u not in visited and self._G[v][u]['weight'] > threshold:
-                parziale.append(u)
-                visited.append(u)
-                self._ricorsione(parziale, visited, threshold)
+    def get_disponibili(self, n, archi, threshold):
+        disponibili = []
+        for u, v, data in self._G.out_edges(n, data=True):
+            if data['weight'] > threshold:
+                if (u, v) not in [(x[0], x[1]) for x in archi]:
+                    disponibili.append(v)
 
-                visited.pop()
-                parziale.pop()
+        return disponibili
 
-    def _calcola_peso(self, parziale):
+    def _calcola_peso(self, sequenza):
         costo = 0
-        if len(parziale) < 2:
-            return 0
-
-        for i in range(len(parziale)-1):
-            v = parziale[i]
-            u = parziale[i+1]
-            costo += self._G[v][u]['weight']
+        for e in sequenza :
+            costo += e[2]['weight']
 
         return costo
